e791775c283b6c239b52089afe3f5e3fc7ffa4d2
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-09-14 15:29:33 +0000

    Module 2.1 created interactive cent to inch program

--- main.cs
+++ main.cs
@@ -2,6 +2,28 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine("Please Enter An Inch Measurement: ");
+    
+       const double CentimetersPerInch = 2.54;
+    string input = Console.ReadLine();
+ 
+    double inches;
+
+    if (double.TryParse(input, out inches))
+    {
+    double centimeters = inches * CentimetersPerInch;
+
+    
+    Console.WriteLine($"{inches} inches is {centimeters} centimeters");
+      }
+    else
+    {
+      Console.WriteLine("Invalid input. ", "Enter a valid number.");
+    }
+  
+  }
+
+  private static string ConcatName(string firstName, string lastName) {
+    return $"{firstName} {lastName}";
   }
 }
